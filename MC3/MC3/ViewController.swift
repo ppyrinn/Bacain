13d83ff09cf19e79ceca0ad8481afab2465c3cb6
Poppy <37153363+ppyrinn@users.noreply.github.com>
2020-07-14 10:08:33 +0700

    Create Xcode Project

---
+++ MC3/MC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC3
+//
+//  Created by Poppy on 14/07/20.
+//  Copyright © 2020 Poppy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
