e2d145e243adc4687e90347195c649797ddc6df0
muhamadVi <jayasriwi@gmail.com>
2020-07-20 22:10:20 +0700

    merge with vicky's progresif view

--- MC3-SwiftUI/MC3-SwiftUI/Progresif/ProgresifView.swift
+++ MC3-SwiftUI/MC3-SwiftUI/Progresif/ProgresifView.swift
@@ -13,47 +13,68 @@ struct ProgresifView: View {
     @FetchRequest(fetchRequest: Sekolah.getAllSekolah()) var listOfSekolah: FetchedResults<Sekolah>
     
     @State private var newSekolah = ""
+    @State var showingDetail = false
     
     var body: some View {
        NavigationView {
+        
+//                contentRoot()
+                
                 List{
-                    Section(header: Text("Tambah Sekolah Baru")) {
-                        HStack{
-                            TextField("Sekolah Baru", text: self.$newSekolah)
-                            Button(action: {
-                                let sekolah = Sekolah(context: self.moc)
-                                sekolah.namaSekolah = self.newSekolah
-                                let uuid = UUID()
-                                sekolah.idSekolah = uuid
-                                
-                                do{
-                                    try self.moc.save()
-                                }catch{
-                                    print(error)
-                                }
-                                
-                                self.newSekolah = ""
-                            }) {
-                                Image(systemName: "plus.circle.fill")
-                                    .foregroundColor(.green)
-                                    .imageScale(.large)
-                            }
-                        }
-                        .font(.headline)
-                    }
+//                    Section(header: Text("Tambah Sekolah Baru")) {
+//                        HStack{
+//                            TextField("Sekolah Baru", text: self.$newSekolah)
+//                            Button(action: {
+//                                let sekolah = Sekolah(context: self.moc)
+//                                sekolah.namaSekolah = self.newSekolah
+//
+//                                do{
+//                                    try self.moc.save()
+//                                }catch{
+//                                    print(error)
+//                                }
+//
+//                                self.newSekolah = ""
+//                            }) {
+//                                Image(systemName: "plus.circle.fill")
+//                                    .foregroundColor(.green)
+//                                    .imageScale(.large)
+//                            }
+//                        }
+//                        .font(.headline)
+//                    }
+                    
                     Section(header: Text("Daftar Sekolah")) {
-                        ForEach(self.listOfSekolah, id: \.idSekolah){ item in
+                        ForEach(self.listOfSekolah, id: \.namaSekolah){ item in
                             NavigationLink(destination: pageDetail(sekolah: item)) {
                                     Text("\(item.namaSekolah)")
 
                             }
+                            .padding(15)
+                            .background(Color.orange)
+                            .cornerRadius(15)
 
                         }
                         .onDelete(perform: deleteItem)
                     }
             }
+            .onAppear {
+                UITableView.appearance().separatorStyle = .none
+            }
+                    
             .navigationBarTitle("Progresif")
-            .navigationBarItems(trailing: EditButton())
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingDetail.toggle()
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                    .foregroundColor(.orange)
+                    .imageScale(.large)
+                }.sheet(isPresented: $showingDetail) {
+                    addSchool()
+                }
+                
+            )
         }
     }
     
@@ -69,6 +90,80 @@ struct ProgresifView: View {
         
     }
 }
+
+struct addSchool: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(fetchRequest: Sekolah.getAllSekolah()) var listOfSekolah: FetchedResults<Sekolah>
+    @State private var newSekolah = ""
+    @State var showDetail = true
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                HStack {
+                    
+                    Button(action: {
+                        self.showDetail.toggle()
+                        
+                    }) {
+                        Text("Batal")
+                            .foregroundColor(.orange)
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // show new task view
+                        let sekolah = Sekolah(context: self.moc)
+                        sekolah.namaSekolah = self.newSekolah
+                        
+                        do{
+                            try self.moc.save()
+                        }catch{
+                            print(error)
+                        }
+                        
+                        self.newSekolah = ""
+                        
+                    }) {
+                        Text("Tambah Kelas")
+                            .foregroundColor(.orange)
+                    }
+                    
+                    
+                    }
+                .padding(30)
+                
+                Spacer()
+                
+                HStack{
+                    Text("Tambah Kelas")
+                        .font(.largeTitle)
+                        .foregroundColor(.orange)
+                }
+                
+                HStack{
+                    Text("Tambah Kelas yang ingin kamu simpan perkembangan murdinya")
+                        
+                }
+                
+                HStack{
+                    Text("Kelas")
+                        .foregroundColor(.orange)
+                        .bold()
+                    TextField("Sekolah Baru", text: self.$newSekolah)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding(30)
+                Spacer()
+                
+                    
+                }
+            }
+        }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
