d36b273141dcc45eb0f1c651e809455a44db5811
Reyhan Rifqi <reyhanazzami@gmail.com>
2020-07-27 19:30:03 +0700

    onBoarding finish

---
+++ MC3-SwiftUI/MC3-SwiftUI/OnBoarding/ViewRouter.swift
@@ -0,0 +1,26 @@
+//
+//  ViewRouter.swift
+//  MC3-SwiftUI
+//
+//  Created by Reyhan Rifqi on 27/07/20.
+//  Copyright © 2020 Poppy. All rights reserved.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ViewRouter: ObservableObject {
+
+    init() {
+        if !UserDefaults.standard.bool(forKey: "didLaunchBefore") {
+            UserDefaults.standard.set(true, forKey: "didLaunchBefore")
+            currentPage = "onboardingView"
+        } else {
+            currentPage = "homeView"
+        }
+    }
+    
+    @Published var currentPage: String
+    
+}
--- MC3-SwiftUI/MC3-SwiftUI/SceneDelegate.swift
+++ MC3-SwiftUI/MC3-SwiftUI/SceneDelegate.swift
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = contentRoot().environment(\.managedObjectContext, context)
+        let contentView = MotherView(viewRouter: ViewRouter()).environment(\.managedObjectContext, context).environmentObject(ViewRouter())
         
 
         // Use a UIHostingController as window root view controller.
