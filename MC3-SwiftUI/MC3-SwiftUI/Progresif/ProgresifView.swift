c8ad3e0dc8104b1c42ac760623db0f6334563e5c
muhamadVi <jayasriwi@gmail.com>
2020-07-27 10:12:55 +0700

    "Nambahin Fitur Progresif update"

--- MC3-SwiftUI/MC3-SwiftUI/Progresif/ProgresifView.swift
+++ MC3-SwiftUI/MC3-SwiftUI/Progresif/ProgresifView.swift
@@ -14,67 +14,63 @@ struct ProgresifView: View {
     
     @State private var newSekolah = ""
     @State var showingDetail = false
+    @State private var searchText = ""
     
     var body: some View {
        NavigationView {
         
 //                contentRoot()
                 
-                List{
-//                    Section(header: Text("Tambah Sekolah Baru")) {
-//                        HStack{
-//                            TextField("Sekolah Baru", text: self.$newSekolah)
-//                            Button(action: {
-//                                let sekolah = Sekolah(context: self.moc)
-//                                sekolah.namaSekolah = self.newSekolah
-//
-//                                do{
-//                                    try self.moc.save()
-//                                }catch{
-//                                    print(error)
-//                                }
-//
-//                                self.newSekolah = ""
-//                            }) {
-//                                Image(systemName: "plus.circle.fill")
-//                                    .foregroundColor(.green)
-//                                    .imageScale(.large)
-//                            }
-//                        }
-//                        .font(.headline)
-//                    }
-                    
-                    Section(header: Text("Daftar Sekolah")) {
-                        ForEach(self.listOfSekolah, id: \.namaSekolah){ item in
-                            NavigationLink(destination: pageDetail(sekolah: item)) {
-                                    Text("\(item.namaSekolah)")
-
-                            }
-                            .padding(15)
-                            .background(Color.orange)
-                            .cornerRadius(15)
-
-                        }
-                        .onDelete(perform: deleteItem)
+        List{
+            Section(header: Text("Daftar Sekolah")) {
+                ForEach(self.listOfSekolah, id: \.namaSekolah){ item in
+                    NavigationLink(destination: pageDetail(sekolah: item)) {
+                        Text("\(item.namaSekolah)")
+                        
                     }
-            }
-            .onAppear {
-                UITableView.appearance().separatorStyle = .none
-            }
+                    .padding(15)
+                    .background(Color(red: 1.00, green: 0.81, blue: 0.42))
+                    .cornerRadius(25)
                     
-            .navigationBarTitle("Progresif")
-            .navigationBarItems(trailing:
-                Button(action: {
-                    self.showingDetail.toggle()
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                    .foregroundColor(.orange)
-                    .imageScale(.large)
-                }.sheet(isPresented: $showingDetail) {
-                    addSchool()
                 }
+                .onDelete(perform: deleteItem)
+            }
+        }
+        .onAppear {
+            UITableView.appearance().separatorStyle = .none
+        }
+            
+        .navigationBarTitle("Progresif")
+        
+        .navigationBarItems(trailing:
+            Button(action: {
+                self.showingDetail.toggle()
+            }) {
+                Image(systemName: "plus")
+                    .foregroundColor(Color(red: 0.79, green: 0.26, blue: 0.0))
+                    .imageScale(.large)
+
+            }.sheet(isPresented: $showingDetail) {
+                addSchool()
+                    .environment(\.managedObjectContext, self.moc)
+            }
+            
+        )
+        
+        ZStack{
+            VStack{
+                Color(red: 1.00, green: 0.81, blue: 0.42)
+                    .padding(.bottom, -20)
+                Image("tambah-daftarkelas")
+                    .padding(.top, -550)
+                Text("Kamu Belum Memilih/Memiliki Sekolah. Pilih/Tambah Sekolah Terlebih Dulu")
+                    .bold()
+                    .padding(.top, -200)
+            }
+        .navigationBarTitle("Daftar Kelas")
+        }
+        
                 
-            )
         }
     }
     
@@ -92,7 +88,6 @@ struct ProgresifView: View {
 }
 
 struct addSchool: View {
-    
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(fetchRequest: Sekolah.getAllSekolah()) var listOfSekolah: FetchedResults<Sekolah>
     @State private var newSekolah = ""
@@ -102,38 +97,104 @@ struct addSchool: View {
         ZStack{
             VStack{
                 HStack {
-                    
                     Button(action: {
                         self.showDetail.toggle()
-                        
                     }) {
                         Text("Batal")
                             .foregroundColor(.orange)
                     }
-                    
-                    
                     Spacer()
                     
                     Button(action: {
-                        // show new task view
                         let sekolah = Sekolah(context: self.moc)
                         sekolah.namaSekolah = self.newSekolah
+                        let id = UUID()
+                        sekolah.idSekolah = id
                         
                         do{
                             try self.moc.save()
                         }catch{
                             print(error)
                         }
-                        
                         self.newSekolah = ""
                         
                     }) {
-                        Text("Tambah Kelas")
+                        Text("Tambah Sekolah")
+                            .foregroundColor(Color(red: 0.79, green: 0.26, blue: 0.0))
+                    }
+                    
+                    
+                    }
+                .padding(30)
+                
+                Spacer()
+                
+                HStack{
+                    Text("Tambah Sekolah")
+                        .font(.largeTitle)
+                        .foregroundColor(.orange)
+                }
+                HStack{
+                    Text("Tambah Sekolah yang ingin kamu simpan/track perkembangan murdinya")
+                        
+                }
+                HStack{
+                    Text("Sekolah")
+                    .foregroundColor(.orange)
+                    .bold()
+                    TextField("Sekolah Baru", text: self.$newSekolah)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .onAppear {
+                        UITableView.appearance().separatorStyle = .singleLine
+                    }
+                }
+                .padding(30)
+                Spacer()
+                }
+            }
+        }
+    }
+
+struct addClass: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(fetchRequest: Sekolah.getAllSekolah()) var listOfSekolah: FetchedResults<Sekolah>
+    @State private var newKelas = ""
+    @State var showDetail = true
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                HStack {
+                    Button(action: {
+                        self.showDetail.toggle()
+                    }) {
+                        Text("Batal")
                             .foregroundColor(.orange)
                     }
+
+                    Spacer()
                     
-                    
+                    Button(action: {
+                        let sekolah = Sekolah(context: self.moc)
+
+                        sekolah.namaSekolah = self.newKelas
+                        let id = UUID()
+                        sekolah.idSekolah = id
+                        
+                        do{
+                            try self.moc.save()
+                        }catch{
+                            print(error)
+                        }
+                        self.newKelas = ""
+                        
+                    }) {
+                        Text("Tambah Kelas")
+                            .foregroundColor(Color(red: 0.79, green: 0.26, blue: 0.0))
                     }
+                    
+                    
+                }
                 .padding(30)
                 
                 Spacer()
@@ -143,27 +204,25 @@ struct addSchool: View {
                         .font(.largeTitle)
                         .foregroundColor(.orange)
                 }
-                
                 HStack{
-                    Text("Tambah Kelas yang ingin kamu simpan perkembangan murdinya")
-                        
+
+                    Text("Tambah Kelas yang ingin kamu simpan/track perkembangan murdinya")
+                    
                 }
-                
                 HStack{
                     Text("Kelas")
                         .foregroundColor(.orange)
                         .bold()
-                    TextField("Sekolah Baru", text: self.$newSekolah)
+                    TextField("Kelas Baru", text: self.$newKelas)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
                 .padding(30)
                 Spacer()
-                
-                    
-                }
             }
         }
     }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MC3-SwiftUI/MC3-SwiftUI/pageDetail.swift
+++ MC3-SwiftUI/MC3-SwiftUI/pageDetail.swift
@@ -8,34 +8,187 @@
 
 import SwiftUI
 
+
 struct pageDetail: View {
     var sekolah: Sekolah
-
+    
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(fetchRequest: Kelas.getAllKelas()) var listOfKelas: FetchedResults<Kelas>
+    //    @FetchRequest(fetchRequest: Kelas.getKelasWithId(id: sekolah.idSekolah)) var listOfKelas: FetchedResults<Kelas>
+    
+    @State var showingDetail = false
+    @State private var searchText = ""
+    
+    @State var data = [
+        
+        Type(namaKelas: "Kelas 1", gambarKelas: "1"),
+        Type(namaKelas: "Kelas 2", gambarKelas: "2"),
+        Type(namaKelas: "Kelas 3", gambarKelas: "3"),
+        Type(namaKelas: "Kelas 4", gambarKelas: "4"),
+        Type(namaKelas: "Kelas 5", gambarKelas: "5"),
+//        Type(namaKelas: "Kelas 6", gambarKelas: "6"),
+
+    ]
+    
+    @State var Grid : [Int] = []
+    
 
     var body: some View {
-        HStack{
-            VStack{
-                
-                Text("\(self.sekolah.idSekolah)")
-
-                Text("\(sekolah.namaSekolah)")
-                
-
+        VStack{
+            HStack{
+                SearchBar(text: $searchText)
             }
-            List{
-                ForEach(self.listOfKelas, id: \.idKelas){ item in
-                        Text("\(item.namaKelas)")
+            
+            VStack(spacing: 0){
+                MainView(data: self.$data, Grid: self.$Grid)
+            }
+            .background(Color.black.opacity(0.06).edgesIgnoringSafeArea(.top))
+            .edgesIgnoringSafeArea(.bottom)
+            .onAppear {
+                
+                self.generateGrid()
+            }
+        }
+        
+            
+        
+        
+        
+        
+//        ZStack{
+//            VStack{
+//                HStack{
+//                    Text("Hello")
+//                    Text("\(self.sekolah.idSekolah)")
+//                }
+//            }
+//        }
+    
+            
+//                                Text("\(sekolah.namaSekolah)")
+//
+//
+//                            }
+//                            List{
+//                                ForEach(self.listOfKelas, id: \.idKelas){ item in
+//                                        Text("\(item.namaKelas)")
+//                                }
+//                            }
+//                        }
+            .navigationBarTitle("Daftar Kelas")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingDetail.toggle()
+                }) {
+                    Image(systemName: "plus")
+                        .foregroundColor(.orange)
+                        .imageScale(.large)
+                }.sheet(isPresented: $showingDetail) {
+                    addClass()
+                        .environment(\.managedObjectContext, self.moc)
                 }
+        )
+        
+        
+    }
+    
+    func generateGrid(){
+        
+        for i in stride(from: 0, to: self.data.count, by: 2){
+            
+            if i != self.data.count{
+                
+                self.Grid.append(i)
             }
+            
         }
     }
     
-    func getKelas(){
+}
+
+struct Card : View {
+    
+    var data : Type
+    
+    var body: some View{
         
+        VStack(spacing: 15){
+            
+            Button(action: {
+                
+            }) {
+                VStack{
+                    Text(data.namaKelas)
+//                        .bold()
+                        .foregroundColor(.black)
+                        .frame(width: (UIScreen.main.bounds.width - 70) / 3)
+                        .padding(.vertical,10)
+                        .padding(.top, 10)
+                        
+                    
+                    Image(data.gambarKelas)
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: (UIScreen.main.bounds.width - 45) / 3)
+                        .cornerRadius(12)
+                        .padding(.bottom, 10)
+                        
+                }
+                
+                
+            }.background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 6)
+            
+        }.padding(5)
     }
 }
+
+struct MainView : View {
+    
+    @Binding var data : [Type]
+    @Binding var Grid : [Int]
+    var body: some View{
+        VStack{
+            if !self.Grid.isEmpty {
+                ScrollView(.vertical, showsIndicators: false){
+                    
+                    ForEach(self.Grid,id: \.self){i in
+                        
+                        HStack(spacing: 15){
+                            
+                            ForEach(i...i+1,id: \.self){j in
+                                
+                                VStack{
+                                    if j != self.data.count {
+                                        Card(data: self.data[j])
+                                    }
+                                }
+                            }
+                            
+                            if i == self.Grid.last! && self.data.count % 2 != 0{
+
+                                Spacer(minLength: 0)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                .background(Color(red: 1.00, green: 0.81, blue: 0.42))
+            }
+            
+          
+        }
+        
+    }
+    
+}
+
+struct Type {
+    
+    var namaKelas : String
+    var gambarKelas : String
+}
+
 
 struct pageDetail_Previews: PreviewProvider {
     static var previews: some View {
