40247b3747821f8b96940f57a84f4f632706f117
Reyhan Rifqi <reyhanazzami@gmail.com>
2020-08-04 23:49:04 +0700

    add jawaban.swift

---
+++ MC3-SwiftUI/MC3-SwiftUI/CoreData/Jawaban.swift
@@ -0,0 +1,29 @@
+//
+//  Jawaban.swift
+//  MC3-SwiftUI
+//
+//  Created by Reyhan Rifqi on 04/08/20.
+//  Copyright © 2020 Poppy. All rights reserved.
+//
+
+import CoreData
+
+class Jawaban: NSManagedObject, Identifiable{
+    @NSManaged public var idJawaban:  UUID
+    @NSManaged public var idKuis:  UUID
+    @NSManaged public var ejaan: String
+    @NSManaged public var score: Int64
+}
+
+
+extension Jawaban{
+    static func getAllJawaban() -> NSFetchRequest<Jawaban>{
+        let request: NSFetchRequest<Jawaban> = Jawaban.fetchRequest() as! NSFetchRequest<Jawaban>
+        
+        let sort = NSSortDescriptor(key: "tanggalKuis", ascending: true)
+        
+        request.sortDescriptors = [sort]
+    
+        return request
+    }
+}
