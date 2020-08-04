305bded337ddc11948e781019be86d7a89aa4271
Reyhan Rifqi <reyhanazzami@gmail.com>
2020-08-04 23:49:04 +0700

    add jawaban.swift

--- MC3-SwiftUI/MC3-SwiftUI/CoreData/Kuis.swift
+++ MC3-SwiftUI/MC3-SwiftUI/CoreData/Kuis.swift
@@ -28,23 +28,3 @@ extension Kuis{
         return request
     }
 }
-
-class Jawaban: NSManagedObject, Identifiable{
-    @NSManaged public var idJawaban:  UUID
-    @NSManaged public var idKuis:  UUID
-    @NSManaged public var ejaan: String
-    @NSManaged public var score: Int64
-}
-
-
-extension Jawaban{
-    static func getAllJawaban() -> NSFetchRequest<Jawaban>{
-        let request: NSFetchRequest<Jawaban> = Jawaban.fetchRequest() as! NSFetchRequest<Jawaban>
-        
-        let sort = NSSortDescriptor(key: "tanggalKuis", ascending: true)
-        
-        request.sortDescriptors = [sort]
-    
-        return request
-    }
-}
